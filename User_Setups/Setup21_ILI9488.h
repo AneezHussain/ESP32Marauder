71efb7961a79e21967689a59cec0be8f58a55d86
Mohammed Aneez <128913614+AneezHussain@users.noreply.github.com>
2025-04-07 23:21:24 +0530

    Update Setup21_ILI9488.h

--- User_Setups/Setup21_ILI9488.h
+++ User_Setups/Setup21_ILI9488.h
@@ -22,6 +22,20 @@
 #define TOUCH_DIN 23  // TD1 -> Data in pin
 #define TOUCH_DOUT 19 // TD0 -> Data out pin
 
+#define LOAD_GLCD   // Font 1. Original Adafruit 8 pixel font needs ~1820 bytes in FLASH
+#define LOAD_FONT2  // Font 2. Small 16 pixel high font, needs ~3534 bytes in FLASH, 96 characters
+#define LOAD_FONT4  // Font 4. Medium 26 pixel high font, needs ~5848 bytes in FLASH, 96 characters
+#define LOAD_FONT6  // Font 6. Large 48 pixel font, needs ~2666 bytes in FLASH, only characters 1234567890:-.apm
+#define LOAD_FONT7  // Font 7. 7 segment 48 pixel font, needs ~2438 bytes in FLASH, only characters 1234567890:-.
+#define LOAD_FONT8  // Font 8. Large 75 pixel font needs ~3256 bytes in FLASH, only characters 1234567890:-.
+//#define LOAD_FONT8N // Font 8. Alternative to Font 8 above, slightly narrower, so 3 digits fit a 160 pixel TFT
+#define LOAD_GFXFF  // FreeFonts. Include access to the 48 Adafruit_GFX free fonts FF1 to FF48 and custom fonts
+
+// Comment out the #define below to stop the SPIFFS filing system and smooth font code being loaded
+// this will save ~20kbytes of FLASH
+#define SMOOTH_FONT
+
+
 // SPI clock frequency settings
 #define SPI_FREQUENCY  27000000 // Actually sets it to 26.67MHz = 80/3
 #define SPI_READ_FREQUENCY  20000000
