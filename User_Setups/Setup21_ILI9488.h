16ade2f3a16140b29ad83f293a8ba42d88028e16
Mohammed Aneez <128913614+AneezHussain@users.noreply.github.com>
2025-04-07 22:57:10 +0530

    Update Setup21_ILI9488.h

--- User_Setups/Setup21_ILI9488.h
+++ User_Setups/Setup21_ILI9488.h
@@ -1,3 +1,3 @@
 #define TFT_WIDTH  320  // Set this to your display's width
-#define TFT_HEIGHT 240  // Set this to your display's height
+#define TFT_HEIGHT 480  // Set this to your display's height
 // Add other necessary configurations here
